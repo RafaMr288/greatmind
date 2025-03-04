4de39d459593fc3439625f5208f110e5e2797f9d
unknown <rafaelmr3453@gmail.com>
2025-03-04 17:43:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Img_greatmind from "./imgs/greatmind.png";
 import Img_solana from "./imgs/solana.png";
 import Img_bitcoin from "./imgs/bitcoin.png";
+import Img_fe from "./imgs/fe.png"
 import "./styles.css";
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
           <div className="menu">
             <a href="#roadmap">ROADMAP</a>
             <a href="#tokenomics">TOKENOMICS</a>
-            <a href="#about">ABOUT</a>
+            <a href="#buy">HOW BUY</a>
           </div>
         </nav>
       </header>
@@ -69,8 +70,17 @@ function App() {
             using smart contracts to support tech and educational projects in a decentralized ecosystem.
           </p>
           <div className="supply" id="tokenomics">
-            <h1>TOKENOMICS</h1>
-            <p>TOTAL SUPPLY: 1,000,000,000</p>
+            <div>
+              <h1>TOKENOMICS</h1>
+              <p>TOTAL SUPPLY: 1,000,000,000</p>
+            </div>
+            <div>
+              <h1>TOKEN ADDRESS</h1>
+              <p>Launch</p>
+              <div>
+                <button>COPY</button>
+              </div>
+            </div>
           </div>
           <div className="roadmap-1" id="roadmap">
             <h1>Phases</h1>
@@ -83,6 +93,33 @@ function App() {
               )
             )}
           </div>
+        </div>
+      </div>
+      <div className="howbuy">
+        <h1>HOW BUY</h1>
+        <div>
+          <div>
+            <h3>1.Create a wallet with Phantom</h3>
+            <p>Visit phantom.App and follow the simple steps to create a new account with the phantom app or browser extension.</p>
+          </div>
+          <div>
+            <h3>2.Get some $SOL</h3>
+            <p>Tap the BUY button in the app to purchase Solana, or deposit $SOL to your Phantom wallet from the crypto exchange of your choice.</p>
+          </div>
+          <div>
+            <h3>3.Swap $SOL for $MIND</h3>
+            <p>Tap the SWAP icon in your Phantom wallet and paste the $MIND token address. Swap your $SOL for $MIND.</p>
+          </div>
+          <div>
+            <h3>4.You are now an $MIND holder!</h3>
+            <p>Welcome to the $MIND Squad!</p>
+          </div>
+        </div>
+      </div>
+      <div className="exchanges">
+        <h2>FUTURE EXCHANGES</h2>
+        <div>
+            <img src={Img_fe}></img>
         </div>
       </div>
       <footer>
